74e1e34976ff0c70feb16c2e93dc4de37fadeeb1
Miguel Montoya <migumont644@ed.amdsb.ca>
2022-05-06 08:57:38 -0400

    changes

--- BrickBreaker/Screens/MenuScreen.Designer.cs
+++ BrickBreaker/Screens/MenuScreen.Designer.cs
@@ -28,20 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuScreen));
             this.playButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.howToButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // playButton
             // 
-            this.playButton.BackColor = System.Drawing.Color.White;
+            this.playButton.BackColor = System.Drawing.Color.Black;
             this.playButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.playButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
             this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
             this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.playButton.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.playButton.Location = new System.Drawing.Point(276, 238);
-            this.playButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.playButton.Font = new System.Drawing.Font("OCR A Extended", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.playButton.Location = new System.Drawing.Point(218, 480);
+            this.playButton.Margin = new System.Windows.Forms.Padding(4);
             this.playButton.Name = "playButton";
             this.playButton.Size = new System.Drawing.Size(242, 96);
             this.playButton.TabIndex = 0;
@@ -51,12 +55,13 @@
             // 
             // exitButton
             // 
-            this.exitButton.BackColor = System.Drawing.Color.White;
+            this.exitButton.BackColor = System.Drawing.Color.Black;
             this.exitButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.exitButton.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.exitButton.Location = new System.Drawing.Point(276, 343);
-            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Font = new System.Drawing.Font("OCR A Extended", 24F, System.Drawing.FontStyle.Bold);
+            this.exitButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.exitButton.Location = new System.Drawing.Point(568, 480);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4);
             this.exitButton.Name = "exitButton";
             this.exitButton.Size = new System.Drawing.Size(242, 96);
             this.exitButton.TabIndex = 1;
@@ -64,17 +69,50 @@
             this.exitButton.UseVisualStyleBackColor = false;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.SystemColors.MenuText;
+            this.titleLabel.Font = new System.Drawing.Font("OCR A Extended", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.titleLabel.Location = new System.Drawing.Point(284, 10);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(427, 83);
+            this.titleLabel.TabIndex = 2;
+            this.titleLabel.Text = "Among Us";
+            // 
+            // howToButton
+            // 
+            this.howToButton.BackColor = System.Drawing.Color.Black;
+            this.howToButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.howToButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.howToButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.howToButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.howToButton.Font = new System.Drawing.Font("OCR A Extended", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.howToButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.howToButton.Location = new System.Drawing.Point(343, 605);
+            this.howToButton.Margin = new System.Windows.Forms.Padding(4);
+            this.howToButton.Name = "howToButton";
+            this.howToButton.Size = new System.Drawing.Size(323, 55);
+            this.howToButton.TabIndex = 3;
+            this.howToButton.Text = "How To Play";
+            this.howToButton.UseVisualStyleBackColor = false;
+            // 
             // MenuScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.Controls.Add(this.howToButton);
+            this.Controls.Add(this.titleLabel);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.playButton);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MenuScreen";
             this.Size = new System.Drawing.Size(1067, 677);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,5 +120,7 @@
 
         private System.Windows.Forms.Button playButton;
         private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button howToButton;
     }
 }
--- BrickBreaker/Screens/MenuScreen.cs
+++ BrickBreaker/Screens/MenuScreen.cs
@@ -7,19 +7,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace BrickBreaker
 {
     public partial class MenuScreen : UserControl
     {
+
+        List<Rectangle> SquareList = new List<Rectangle>(); //Made List for squares (JUAN)
+        Random randGen = new Random();
+        Size screenSize;
+
         public MenuScreen()
         {
             InitializeComponent();
+            Juanmethod_FlyingSquare();
         }
 
         private void exitButton_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        public void Juanmethod_FlyingSquare() //Method meant to create flying squares (JUAN)
+        {
+
+            for (int i = 0; i < 5; i++) //for loop to create the limit of square
+            {
+                JuanMethod_NewFlyingSquares();
+            }
+
+            Juanmethod_MovingSquares(); //Declares the move method
+        }
+
+        public void JuanMethod_NewFlyingSquares()  //method meant for creating and putting the squares into a list (JUAN)
+        {
+
+
+
+            screenSize = new Size(this.Width, this.Height);
+
+         int x = randGen.Next(40, screenSize.Width - 40);
+         int y = randGen.Next(40, screenSize.Height - 40);
+
+         Rectangle AmongOne = new Rectangle(x, y, 8, 8);
+            SquareList.Add(AmongOne);
+
+        }
+
+        public void Juanmethod_MovingSquares() //Method meant for moving the squares (JUAN)
+        {
+            int xspeed = 4;
+            for (int i = 1; i < 0; i++)
+            {
+                square.x = square.x + xspeed;
+            }
+
+            if (SquareList[i] <= 1067)
+            {
+                int x = randGen.Next(40, screenSize.Width - 40);
+            }
         }
 
         private void playButton_Click(object sender, EventArgs e)
